9330063409d0472d26e59969298a8156f28220a7
Andreas Klintberg <andreas.klintberg@meltwater.com>
2016-07-07 15:01:24 +0200

    Now supports NLTK 3.0 and Stanford 3.6.0

--- svo.py
+++ svo.py
@@ -20,13 +20,13 @@ class SVO(object):
     """
     def __init__(self):
         """
-        Initialize the SVO Methods 
+        Initialize the SVO Methods
         """
         self.noun_types = ["NN", "NNP", "NNPS","NNS","PRP"]
         self.verb_types = ["VB","VBD","VBG","VBN", "VBP", "VBZ"]
         self.adjective_types = ["JJ","JJR","JJS"]
         self.pred_verb_phrase_siblings = None
-        self.parser = stanford.StanfordParser(model_path="")
+        self.parser = stanford.StanfordParser()
         self.sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 
     def get_attributes(self,node,parent_node, parent_node_siblings):
@@ -38,7 +38,7 @@ class SVO(object):
         """
         Returns the Subject and all attributes for a subject, sub_tree is a Noun Phrase
         """
-        sub_nodes = []      
+        sub_nodes = []
         sub_nodes = sub_tree.subtrees()
         sub_nodes = [each for each in sub_nodes if each.pos()]
         subject = None
@@ -51,7 +51,7 @@ class SVO(object):
 
         return {'subject':subject}
 
-    def get_object(self,sub_tree):        
+    def get_object(self,sub_tree):
         """
         Returns an Object with all attributes of an object
         """
@@ -64,7 +64,7 @@ class SVO(object):
 
                 for each in sub_nodes:
                     if each.label() in self.noun_types:
-                        Object = each.leaves()        
+                        Object = each.leaves()
                         break
                 break
             else:
@@ -72,44 +72,44 @@ class SVO(object):
                 sub_nodes = [each for each in sub_nodes if each.pos()]
                 for each in sub_nodes:
                     if each.label() in self.adjective_types:
-                        Object = each.leaves()        
-                        break                        
-                # Get first noun in the tree                             
+                        Object = each.leaves()
+                        break
+                # Get first noun in the tree
         self.pred_verb_phrase_siblings = None
-        return {'object':Object}        
+        return {'object':Object}
 
     def get_predicate(self, sub_tree):
         """
         Returns the Verb along with its attributes, Also returns a Verb Phrase
         """
 
-        sub_nodes = []      
+        sub_nodes = []
         sub_nodes = sub_tree.subtrees()
-        sub_nodes = [each for each in sub_nodes if each.pos()]        
+        sub_nodes = [each for each in sub_nodes if each.pos()]
         predicate = None
         pred_verb_phrase_siblings = []
         sub_tree  = ParentedTree.convert(sub_tree)
         for each in sub_nodes:
             if each.label() in self.verb_types:
                 sub_tree = each
-                predicate = each.leaves() 
-        
+                predicate = each.leaves()
+
         #get all predicate_verb_phrase_siblings to be able to get the object
         sub_tree  = ParentedTree.convert(sub_tree)
         if predicate:
              pred_verb_phrase_siblings = self.tree_root.subtrees()
-             pred_verb_phrase_siblings = [each for each in pred_verb_phrase_siblings if each.label() in ["NP","PP","ADJP","ADVP"]]        
+             pred_verb_phrase_siblings = [each for each in pred_verb_phrase_siblings if each.label() in ["NP","PP","ADJP","ADVP"]]
              self.pred_verb_phrase_siblings = pred_verb_phrase_siblings
 
-        return {'predicate':predicate}                        
-        
+        return {'predicate':predicate}
+
     def process_parse_tree(self,parse_tree):
         """
         Returns the Subject-Verb-Object Representation of a Parse Tree.
         Can Vary depending on number of 'sub-sentences' in a Parse Tree
-        """              
+        """
         self.tree_root = parse_tree
-        # Step 1 - Extract all the parse trees that start with 'S'        
+        # Step 1 - Extract all the parse trees that start with 'S'
         svo_list = [] # A List of SVO pairs extracted
         output_list = []
         output_dict ={}
@@ -121,7 +121,7 @@ class SVO(object):
             Object = None
             if subtree.label() in ["S", "SQ", "SBAR", "SBARQ", "SINV", "FRAG"]:
                 children_list = subtree
-                children_values = [each_child.label() for each_child in children_list]               
+                children_values = [each_child.label() for each_child in children_list]
                 children_dict = dict(zip(children_values,children_list))
 
 
@@ -143,17 +143,17 @@ class SVO(object):
                     if subject['subject'] and predicate['predicate'] and Object['object']:
                         output_dict['subject'] = subject['subject']
                         output_dict['predicate'] = predicate['predicate']
-                        output_dict['object'] = Object['object']                                         
+                        output_dict['object'] = Object['object']
                         output_list.append(output_dict)
                 except Exception, e:
                         print e
                         continue
-                
-                    
+
+
 
         return output_list
-                
-     	
+
+
     def traverse(self,t):
         try:
             t.label()
@@ -169,7 +169,7 @@ class SVO(object):
 
     def sentence_split(self,text):
         """
-        returns the Parse Tree of a Sample 
+        returns the Parse Tree of a Sample
         """
         sentences = self.sent_detector.tokenize(text)
         return sentences
@@ -177,7 +177,7 @@ class SVO(object):
 
     def get_parse_tree(self,sentence):
         """
-        returns the Parse Tree of a Sample 
+        returns the Parse Tree of a Sample
         """
         parse_tree = self.parser.raw_parse(sentence)
 
@@ -185,7 +185,7 @@ class SVO(object):
 
     def List_To_Tree(self,lst):
         if(not isinstance(lst, basestring)):
-            if(len(lst) == 2 and isinstance(lst[0], basestring) and isinstance(lst[1], basestring)):                
+            if(len(lst) == 2 and isinstance(lst[0], basestring) and isinstance(lst[1], basestring)):
                 lst = Tree(str(lst[0]).split('+')[0],[str(lst[1])])
             elif(isinstance(lst[0], basestring) and not isinstance(lst[1], basestring)):
                 lst = Tree(str(lst[0]).split('+')[0],map(List_To_Tree, lst[1: len(lst) ]))
@@ -199,8 +199,8 @@ if __name__=="__main__":
     val = []
     for sent in sentences:
         root_tree = svo.get_parse_tree(sent)
-        val.append(svo.process_parse_tree(root_tree[0]))
-        
+        val.append(svo.process_parse_tree(next(root_tree)))
+
     print val
-    
+
 
